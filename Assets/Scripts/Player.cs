17efa775f9c41c8fe5cc23ca32c726bdb3e073b4
brendan-lojam <33971709+brendan-lojam@users.noreply.github.com>
2020-10-03 02:55:49 -0400

    grace time for jumping

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,10 +9,12 @@ public class Player : MonoBehaviour
     
     private bool mOnGround = true;
     private float mJumpTimer = 0f;
+    private float mJumpGraceTimeTimer = 0f;
     private float mSpeed = 5;
 
     private const float GROUND_CHECK_DIST = 0.015f;
     private const int PLATFORM_PHYS_LAYER = 1 << 8;
+    private const float JUMP_GRACE_TIME = 0.15f;
 
     // Start is called before the first frame update
     void Start()
@@ -23,25 +25,8 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        if(mJumpTimer > 0f)
-        {
-            mJumpTimer -= Time.fixedDeltaTime;
-        }
-
-        mOnGround = false;
-        BoxCollider2D collider = GetComponent<BoxCollider2D>();
-        RaycastHit2D[] groundHits = Physics2D.BoxCastAll(transform.position, collider.size, 0, new Vector2(0, -1), GROUND_CHECK_DIST, PLATFORM_PHYS_LAYER);
-        foreach(RaycastHit2D hit in groundHits)
-        {   
-            if(hit.collider != null && hit.normal.y > 0 && hit.distance <= GROUND_CHECK_DIST && hit.collider != collider)
-            {
-                mOnGround = true;
-                break;
-            }
-        }        
-
         Vector2 vel = new Vector2();
-
+        BoxCollider2D collider = GetComponent<BoxCollider2D>();
         float horiz = Input.GetAxis("Horizontal");
         bool sideBlocked = false;
         if(horiz != 0f)
@@ -64,8 +49,36 @@ public class Player : MonoBehaviour
             }
         }
 
+        // jumping
+        bool onGround = false;
+        RaycastHit2D[] groundHits = Physics2D.BoxCastAll(transform.position, collider.size, 0, new Vector2(0, -1), GROUND_CHECK_DIST, PLATFORM_PHYS_LAYER);
+        foreach(RaycastHit2D hit in groundHits)
+        {   
+            if(hit.collider != null && hit.normal.y > 0 && hit.distance <= GROUND_CHECK_DIST && hit.collider != collider)
+            {
+                onGround = true;
+                break;
+            }
+        }
+
+        if(mJumpTimer > 0f)
+        {
+            mJumpTimer -= Time.fixedDeltaTime;
+        }
+
+        if(mJumpGraceTimeTimer > 0f)
+        {
+            mJumpGraceTimeTimer -= Time.fixedDeltaTime;
+        }
+
+        if(mOnGround && !onGround)
+        {
+            // just left the ground
+            mJumpGraceTimeTimer = JUMP_GRACE_TIME;
+        }
+
         bool jump = Input.GetButton("Jump");
-        if(jump && mOnGround && mJumpTimer <= 0f)
+        if(jump && (onGround || mJumpGraceTimeTimer > 0f) && mJumpTimer <= 0f)
         {
             vel.y = jumpSpeed;
             mOnGround = false;
@@ -75,6 +88,8 @@ public class Player : MonoBehaviour
         {
             vel.y = mRigidbody.velocity.y;
         }
+
+        mOnGround = onGround;
 
         mRigidbody.velocity = vel;
     }
