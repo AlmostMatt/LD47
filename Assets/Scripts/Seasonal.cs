1351e39439764b2695a0a787b681015330267ee4
Matthew Hyndman <myndman@gmail.com>
2020-10-03 01:17:22 -0400

    draw gizmos in the scene for where copies of seasonal objects will be

--- Assets/Scripts/Seasonal.cs
+++ Assets/Scripts/Seasonal.cs
@@ -13,3 +13,28 @@ public class Seasonal : MonoBehaviour
     }
 
 }
+
+// Draws a gizmo in the editor to visualize the seasonal copies
+public class SeasonalGizmoDrawer
+{
+    [DrawGizmo(GizmoType.Selected | GizmoType.Active | GizmoType.NotInSelectionHierarchy)]
+    static void DrawGizmoForMyScript(Seasonal scr, GizmoType gizmoType)
+    {
+        Renderer rend = scr.GetComponent<Renderer>();
+        DrawRectangle(Vector3.zero, rend.bounds);
+        DrawRectangle(new Vector3(+SeasonalSystem.SEASONAL_OFFSET, 0f, 0f), rend.bounds);
+        DrawRectangle(new Vector3(-SeasonalSystem.SEASONAL_OFFSET, 0f, 0f), rend.bounds);
+    }
+
+    static void DrawRectangle(Vector3 offset, Bounds bounds)
+    {
+        Vector3 botLeft = offset + bounds.min;
+        Vector3 topRight = offset + bounds.max;
+        Vector3 topLeft = new Vector3(botLeft.x, topRight.y, botLeft.z);
+        Vector3 botRight = new Vector3(topRight.x, botLeft.y, botLeft.z);
+        Gizmos.DrawLine(botLeft, topLeft);
+        Gizmos.DrawLine(topLeft, topRight);
+        Gizmos.DrawLine(topRight, botRight);
+        Gizmos.DrawLine(botRight, botLeft);
+    }
+}
