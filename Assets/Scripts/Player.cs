501e439560074e9df387573df2765a6084134839
brendan-lojam <33971709+brendan-lojam@users.noreply.github.com>
2020-10-02 23:18:33 -0400

    player stuff

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    Rigidbody2D mRigidbody;
+    public float gravity = -9.8f;
+    public float jumpSpeed = 5f;
+    
+    private bool mOnGround = true;
+    private float mSpeed = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 vel = new Vector2();
+
+        float horiz = Input.GetAxis("Horizontal");
+        bool jump = Input.GetButtonDown("Jump");
+
+        if(jump && mOnGround)
+        {
+            vel.y = jumpSpeed;
+        }
+        else
+        {
+            vel.y = mRigidbody.velocity.y;
+        }
+
+        if(horiz != 0f)
+        {
+            vel.x = (Mathf.Sign(horiz) * mSpeed);
+        }
+
+        mRigidbody.velocity = vel;
+    }
+}
