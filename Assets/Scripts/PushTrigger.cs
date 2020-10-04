9c937f25c8da8ed2a518cd0361cf2c9123297302
brendan-lojam <33971709+brendan-lojam@users.noreply.github.com>
2020-10-03 20:52:19 -0400

    chimney elevator to next section

---
+++ Assets/Scripts/PushTrigger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PushTrigger : MonoBehaviour
+{
+    public Vector2 force = new Vector2(0, 1);
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        collider.attachedRigidbody.AddForce(force);
+    }
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        // other.attachedRigidbody.AddForce(force);
+
+        other.attachedRigidbody.velocity = new Vector2(other.attachedRigidbody.velocity.x, Mathf.Max(3, other.attachedRigidbody.velocity.y));
+    }
+}
