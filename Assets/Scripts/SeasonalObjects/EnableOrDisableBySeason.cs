b4df8dda29d3130e04ac58e7ccf0856b05fe4ec0
Matthew Hyndman <myndman@gmail.com>
2020-10-04 17:33:41 -0400

    generalize the Vines script (enables or disables based on season)

---
+++ Assets/Scripts/SeasonalObjects/EnableOrDisableBySeason.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnableOrDisableBySeason : MonoBehaviour
+{
+    public bool[] EnabledBySeason = new bool[4];
+    
+    void Start()
+    {
+        Season season = GetComponent<Seasonal>().Season;
+        gameObject.SetActive(EnabledBySeason[(int)season]);
+    }
+}
